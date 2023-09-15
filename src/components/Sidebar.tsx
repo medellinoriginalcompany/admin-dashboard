5cf63961ad48fb6ce27bd21c214d1251cb4ed25e
Vini <vsgh-gouveia@hotmail.com>
2023-09-15 13:35:59 -0300

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -14,14 +14,17 @@ const Sidebar = () => {
   }
 
   return (
-    <header className="bg-primary fixed h-screen shadow-lg">
-      <nav className='px-8 py-10 flex flex-col justify-between h-full'>
-        <div className='space-y-10'>
-          <Link to='/dashboard'>
-            <img src={logo} alt="Logo" className='w-24' />
-          </Link>
+    <div className="bg-primary sticky top-0 min-w-[250px] h-screen shadow-lg z-50">
+      <nav className='flex flex-col justify-between h-full'>
+        <div className='space-y-7'>
+          <div className='py-8'>
+            <Link to='/dashboard'>
+              <img src={logo} alt="Logo" className='w-24 mx-auto' />
+            </Link>
 
-          <ul className='space-y-4'>
+          </div>
+
+          <ul className='space-y-4 px-8'>
             <li>
               <NavItem
                 icon='graph'
@@ -38,19 +41,21 @@ const Sidebar = () => {
             </li>
           </ul>
         </div>
-        <button
-          className='flex items-center w-full space-x-4 px-8 py-2 rounded-lg group transition-all hover:shadow-lg hover:text-primary hover:bg-accent/90'
-          onClick={handleLogout}>
-          <div className=''>
-            <img src={`/icons/logout.svg`} alt='logout' className='w-7 group-hover:brightness-[6] ' />
-          </div>
+        <div className='px-8 py-10'>
+          <button
+            className='flex items-center w-full space-x-4 px-8 py-2 rounded-lg group transition-all hover:shadow-lg hover:text-primary hover:bg-accent/90'
+            onClick={handleLogout}>
+            <div className=''>
+              <img src={`/icons/logout.svg`} alt='logout' className='w-7 group-hover:brightness-[6] transition-all' />
+            </div>
 
-          <span className='font-semibold'>
-            Sair
-          </span>
-        </button>
+            <span className='font-semibold'>
+              Sair
+            </span>
+          </button>
+        </div>
       </nav>
-    </header>
+    </div>
   )
 }
 
