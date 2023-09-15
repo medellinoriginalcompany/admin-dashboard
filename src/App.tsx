f730c102cee30fcb5c823f98185c4e56dd571643
Vini <vsgh-gouveia@hotmail.com>
2023-09-15 20:36:26 -0300

    Rotas para cadastro de produto e 404

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
 
-import { Routes, Route, Navigate, useLocation } from 'react-router-dom'
+import { Routes, Route, Navigate, useLocation, Link } from 'react-router-dom'
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import LoginGuard from './contexts/VerifyLogin';
 import { RequireAuth } from './contexts/RequireAuth';
 import Products from './pages/Products';
+import AddProduct from './pages/AddProduct';
 
 function App() {
   const location = useLocation();
@@ -16,12 +17,14 @@ function App() {
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/dashboard" element={<RequireAuth><Dashboard /></RequireAuth>} />
         <Route path='/produtos' element={<RequireAuth><Products /></RequireAuth>} />
+        <Route path='/produtos/cadastrar' element={<RequireAuth><AddProduct /></RequireAuth>} />
         <Route path="/login" element={
           <LoginGuard>
             <Login />
           </LoginGuard>
         } />
 
+        <Route path='/404' element={<Link to='/'>404</Link>} />
         <Route path='*' element={<Navigate to='/404' replace />} />
       </Routes>
     </>
