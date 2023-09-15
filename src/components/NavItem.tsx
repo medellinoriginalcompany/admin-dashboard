28754863bd7421d7e595885e958c5efd68489983
Vini <vsgh-gouveia@hotmail.com>
2023-09-15 13:36:23 -0300

    Transition navitem

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -14,7 +14,7 @@ const NavItem = (props: Props) => {
         <>
           <div className={'flex items-center space-x-4 px-8 py-2 rounded-lg transition-all ' + (isActive ? 'bg-accent text-primary shadow-lg' : 'group hover:text-primary hover:bg-accent/80')}>
             <div className=''>
-              <img src={`/icons/${props.icon}.svg`} alt={props.icon} className={'w-7 ' + (isActive ? 'brightness-[6]' : 'group-hover:brightness-[6]')} />
+              <img src={`/icons/${props.icon}.svg`} alt={props.icon} className={'w-7 transition-all ' + (isActive ? 'brightness-[6]' : 'group-hover:brightness-[6]')} />
             </div>
 
             <span className='font-semibold'>
