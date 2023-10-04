dedce4888ceb8cebfcc323e9fe8d0d4b17c2d891
Vini <vsgh-gouveia@hotmail.com>
2023-10-04 11:26:36 -0300

    Bordas table head

--- src/components/table/HeadRow.tsx
+++ src/components/table/HeadRow.tsx
@@ -1,11 +1,13 @@
 
 type Props = {
   content: string,
+  left?: boolean,
+  right?: boolean
 }
 
 const HeadRow = (props: Props) => {
   return (
-    <th className="pl-4 pr-10 py-4 text-left ">{props.content}</th>
+    <th className={"pl-4 pr-10 py-4 text-left text-white font-semibold " + (props.left ? 'rounded-l-lg ' : '') + (props.right ? 'rounded-r-lg ' : '')}>{props.content}</th>
   )
 }
 
