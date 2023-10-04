51a6297099cb1fe8b437864cc7191377d1546ea1
Vini <vsgh-gouveia@hotmail.com>
2023-10-04 00:08:09 -0300

    Create ProductsProperties.tsx

---
+++ src/pages/ProductsProperties.tsx
@@ -0,0 +1,24 @@
+import Header from "../components/Header"
+import Sidebar from "../components/Sidebar"
+
+const ProductsProperties = () => {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <Header />
+      <div className="w-full">
+        <div className="p-8 space-y-5">
+          <h2 className="text-2xl font-light text-neutral-800">
+            Propriedades
+          </h2>
+
+          <div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProductsProperties
