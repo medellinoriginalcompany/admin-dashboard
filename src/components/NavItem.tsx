b8ca66a90145c485b6986c5889f16a707223664a
Vini <vsgh-gouveia@hotmail.com>
2023-10-04 00:11:49 -0300

    CSS update NavItem.tsx

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -15,14 +15,14 @@ const NavItem = (props: Props) => {
         {({ isActive }) => (
           <>
             <div className={'flex items-center justify-between pl-5 py-3 rounded-xl transition-all relative group '
-              + (isActive ? 'bg-accent border-neutral-800 shadow-lg ' : 'border-transparent hover:bg-accent')}>
-              <div className="flex gap-2 items-center">
+              + (isActive ? 'bg-accent border-neutral-800 shadow-lg shadow-accent/30 ' : 'border-transparent hover:bg-accent/80')}>
+              <div className="flex gap-3 items-center">
                 <div className=''>
                   <img src={`/icons/${props.icon}.svg`} alt={props.icon}
                     className={'w-7 transition-all '+(isActive ? 'brightness-[6]' : 'brightness-[2] group-hover:brightness-[6]')} />
                 </div>
 
-                <span className={'font-semibold ' + (isActive ? 'text-white' : 'text-neutral-600 group-hover:text-white')}>
+                <span className={'font-semibold transition-all ' + (isActive ? 'text-white' : 'text-neutral-600 group-hover:text-white')}>
                   {props.text}
                 </span>
               </div>
