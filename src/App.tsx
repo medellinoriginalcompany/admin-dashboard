3513ad0fd5a082d62809df41a568deb7574bbe88
Vini <vsgh-gouveia@hotmail.com>
2023-10-05 07:02:19 -0300

    Rota para pedidos

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { RequireAuth } from './contexts/RequireAuth';
 import Products from './pages/Products';
 import ProductsAdd from './pages/ProductsAdd';
 import ProductsProperties from './pages/ProductsProperties';
+import Orders from './pages/Orders';
 
 function App() {
   const location = useLocation();
@@ -20,6 +21,7 @@ function App() {
         <Route path='/produtos' element={<RequireAuth><Products /></RequireAuth>} />
         <Route path='/produtos/cadastrar' element={<RequireAuth><ProductsAdd /></RequireAuth>} />
         <Route path='/produtos/propriedades' element={<RequireAuth><ProductsProperties /></RequireAuth>} />
+        <Route path='/pedidos' element={<RequireAuth><Orders /></RequireAuth>} />
         <Route path="/login" element={
           <LoginGuard>
             <Login />
