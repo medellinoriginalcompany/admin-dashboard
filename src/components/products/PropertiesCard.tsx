9abde2bf23eaeafe7fbf1f738652acacf8b71698
Vini <vsgh-gouveia@hotmail.com>
2023-10-05 23:04:52 -0300

    Alpha adicionar propriedades

--- src/components/products/PropertiesCard.tsx
+++ src/components/products/PropertiesCard.tsx
@@ -1,50 +1,151 @@
-import { Link } from "react-router-dom"
 import PropertiesRow from "./PropertiesRow"
-import { ProductProperties } from "../../types/product/Properties"
+import { ProductProperty } from "../../types/product/Property"
+import { useState, ChangeEvent } from "react"
+import ProductInput from "../form/ProductInput"
+
+import closeicon from "/icons/close-circle.svg"
+import { useApi } from "../../hooks/useApi"
+import { redirect } from "react-router-dom"
+
 
 type Props = {
   title: string,
-  property: ProductProperties[],
+  property: ProductProperty[],
   loading: boolean,
 }
 
 const PropertiesCard = (props: Props) => {
+
+  const api = useApi();
+
+  const [name, setName] = useState<string>('');
+  const [description, setDescription] = useState<string>('');
+  const [errMessage, setErrMessage] = useState<string>('');
+  const [confirmationMessage, setConfirmationMessage] = useState<string>('');
+
+  const [showAddProperty, setShowAddProperty] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setShowAddProperty(!showAddProperty);
+  }
+
+  async function handleSubmit(e: React.SyntheticEvent) {
+    e.preventDefault();
+    
+    try {
+      const response = await api.addProductProperty(props.title.toLowerCase(), name, description);
+
+      if(response) {
+        setConfirmationMessage(response.data.message);
+        setShowAddProperty(false);
+        redirect('produtos/propriedades', 2000)
+      }
+    } catch (error: any) {
+      setErrMessage(error.response.data.message)
+    }
+  }
+
+
   return (
-    <div className="flex space-x-5 justify-between h-96">
-      <div className="bg-white/60 backdrop-blur rounded-xl py-7 px-10 space-y-5 overflow-y-scroll card w-full">
-        <div className="flex justify-between sticky top-0 backdrop-blur bg-[#F3F6FB]/90 px-3 py-1 rounded-lg z-20">
-          <h3 className="font-semibold text-neutral-700">
-            {props.title}
-          </h3>
+    <>
+      <div className="flex space-x-5 justify-between h-96">
+        <div className="bg-white/60 backdrop-blur rounded-xl py-7 px-10 space-y-5 overflow-y-scroll card w-full">
+          <div className="flex justify-between sticky top-0 backdrop-blur bg-[#F3F6FB]/90 px-3 py-1 rounded-lg z-20">
+            <h3 className="font-semibold text-neutral-700">
+              {props.title}
+            </h3>
 
-          <Link to="/renda/mensal" className="w-fit flex space-x-2">
-            <span>
-              Adicionar {props.title.toLowerCase()}
-            </span>
-            <img src="/icons/arrow-right-1.svg" alt="arrow-right" className="w-4" draggable='false' />
-          </Link>
-        </div>
+            <button onClick={handleClick} className="w-fit flex items-center space-x-2 px-3 rounded hover:bg-accent/20">
+              <span>
+                Adicionar {props.title.toLowerCase()}
+              </span>
+              <img src="/icons/arrow-right-1.svg" alt="arrow-right" className="w-4" draggable='false' />
+            </button>
+          </div>
 
-        <div>
-          <table className="w-full">
-            <thead className="text-left bg-accent backdrop-blur shadow-lg shadow-accent/10 text-white rounded-lg">
-              <tr>
-                <th className="pl-4 px-10  font-medium py-1 rounded-l-lg">
-                  #
-                </th>
-                <th className="px-4 font-medium">
-                  {props.title}
-                </th>
-                <th className="rounded-r-lg"></th>
-              </tr>
-            </thead>
-            <tbody>
-              <PropertiesRow loading={props.loading} property={props.property} type='tamanho' />
-            </tbody>
-          </table>
+          <div>
+            <table className="w-full">
+              <thead className="text-left bg-accent backdrop-blur shadow-lg shadow-accent/10 text-white rounded-lg">
+                <tr>
+                  <th className="pl-4 px-10  font-medium py-1 rounded-l-lg">
+                    #
+                  </th>
+                  <th className="px-4 font-medium">
+                    {props.title}
+                  </th>
+                  <th className="rounded-r-lg"></th>
+                </tr>
+              </thead>
+              <tbody>
+                <PropertiesRow loading={props.loading} property={props.property} type={props.title.toLowerCase()} />
+              </tbody>
+            </table>
+          </div>
+
         </div>
       </div>
-    </div>
+      {
+        showAddProperty && (
+          <div className="absolute top-0 left-0 w-full bg-neutral-800/20 z-50">
+            <div className="flex flex-col justify-center h-screen mx-auto max-w-xl">
+              <div className="bg-white p-10 rounded relative">
+                <div className="absolute top-0 right-0 p-3 cursor-pointer" onClick={handleClick}>
+                  <img src={closeicon} alt="" />
+                </div>
+                <form className="space-y-5" onSubmit={handleSubmit}>
+                  <ProductInput
+                    label={"Adicionar novo (" + props.title + ")"}
+                    name={props.title.toLowerCase()}
+                    type="text"
+                    value={name}
+                    handleOnChange={(e: ChangeEvent<HTMLInputElement>) => { setName(e.target.value) }}
+                    maxLength={50}
+                    placeholder="Digite a nova propriedade"
+                    autoFocus
+                  />
+                  <ProductInput
+                    label="Descrição (não obrigatório)"
+                    name={props.title.toLowerCase()}
+                    type="text"
+                    value={description}
+                    handleOnChange={(e: ChangeEvent<HTMLInputElement>) => { setDescription(e.target.value) }}
+                    maxLength={50}
+                    placeholder="Digite a descrição da propriedade"
+                  />
+
+                  <div className="flex justify-end">
+                    <button type="submit" disabled={!name}
+                      className="bg-accent text-primary rounded-lg w-fit px-8 py-2 font-semibold flex items-center shadow-lg hover:bg-neutral-900">
+                      Cadastrar
+                    </button>
+                  </div>
+                </form>
+                {
+                  confirmationMessage && (
+                    <div className='absolute bottom-0 left-0 m-5'>
+                      <div className="bg-green-100 text-green-500 px-3 py-2 rounded-lg shadow-lg">
+                        <span>{confirmationMessage}</span>
+                      </div>
+                    </div>
+                  )
+                }
+                {
+                  errMessage && (
+                    <div className='absolute bottom-0 left-0 m-5'>
+                      <div className="bg-red-100 text-red-500 px-3 py-2 rounded-lg shadow-lg shadow-red-500/20">
+                        <p>
+                          Erro: <span>{errMessage}</span>
+                        </p>
+                      </div>
+                    </div>
+                  )
+                }
+              </div>
+            </div>
+          </div>
+        )
+      }
+    </>
   )
 }
 
--- src/components/products/PropertiesRow.tsx
+++ src/components/products/PropertiesRow.tsx
@@ -1,13 +1,14 @@
-import { ProductProperties } from '../../types/product/Properties'
+import { ProductProperty } from '../../types/product/Property'
 
 import editicon from '/icons/edit.svg';
 import deleteicon from '/icons/trash.svg';
 import { useApi } from '../../hooks/useApi';
+import { useState } from 'react';
 
 type Props = {
   loading: boolean,
   type: string,
-  property: ProductProperties[],
+  property: ProductProperty[],
 }
 
 const PropertiesRow = (props: Props) => {
@@ -15,14 +16,26 @@ const PropertiesRow = (props: Props) => {
   const api = useApi();
   const type = props.type.toLowerCase();
 
+  const [confirmationMessage, setConfirmationMessage] = useState('');
+  const [errMessage, setErrMessage] = useState('');
+
   const handleDelete = async (id: number, name: string) => {
     if (window.confirm("Tem certeza que deseja excluir a propriedade " + name + "?")) {
       const response = await api.delete(type, id);
       if (response) {
         console.log('Deletado com sucesso');
+        // Mostrar mensagem de confirmação por 3 segundos
+        setConfirmationMessage(response.message);
+        setTimeout(() => {
+          setConfirmationMessage('');
+        }, 3000);
 
       } else {
         console.log("Ocorreu um erro ao excluir");
+        setErrMessage(response.message);
+        setTimeout(() => {
+          setErrMessage('');
+        }, 5000);
       }
     }
   }
@@ -31,11 +44,11 @@ const PropertiesRow = (props: Props) => {
     <>
       {
         props.loading ? (<tr><td>Carregando...</td></tr>) :
-          props.property?.map((param) => {
+          props.property?.map((param, index) => {
             return (
               <tr key={param.ID}>
                 <td className="px-4 py-3">
-                  {param.ID}
+                  {index}
                 </td>
                 <td className="px-4">
                   {param.Name}
@@ -43,9 +56,9 @@ const PropertiesRow = (props: Props) => {
                 <td className="relative">
                   <div className="space-x-2 mx-4 float-right">
                     <button className="bg-sky-200/80 p-2 rounded-full hover:bg-sky-300/80">
-                      <img src={editicon} alt='edit' className='w-3' draggable='false' />
+                      <img src={editicon} alt='edit' className='w-3 ' draggable='false' />
                     </button>
-                    <button onClick={async () => { await handleDelete(param.ID, param.Name); }} className="bg-red-200/80 p-2 rounded-full hover:bg-red-300/80">
+                    <button onClick={() => handleDelete(param.ID, param.Name)} className="bg-red-200/80 p-2 rounded-full hover:bg-red-300/80">
                       <img src={deleteicon} alt='delete' className='w-3' draggable='false' />
                     </button>
                   </div>
@@ -53,6 +66,25 @@ const PropertiesRow = (props: Props) => {
               </tr>
             )
           })
+      }
+
+      {
+        confirmationMessage ? (
+          <div className='absolute bottom-0 right-0'>
+            <div className="bg-green-400 text-white p-2 rounded-lg shadow-lg">
+              {confirmationMessage}
+            </div>
+          </div>
+        ) : null
+      }
+      {
+        errMessage ? (
+          <div className='absolute bottom-0 right-0'>
+            <div className="bg-red-400 text-white p-2 rounded-lg shadow-lg">
+              {errMessage}
+            </div>
+          </div>
+        ) : null
       }
     </>
   )
