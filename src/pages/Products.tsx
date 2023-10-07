accdba1301052c367d1a42dc5d8b2279b33cc4d1
Vini <vsgh-gouveia@hotmail.com>
2023-10-07 02:17:51 -0300

    Refactor Products.tsx
    Produtos organizados de table para list, visual levemente alterado, limitação inicial de produtos mostrados é 50

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,6 +1,5 @@
 import Header from "../components/Header"
 import Sidebar from "../components/Sidebar"
-import HeadRow from "../components/table/HeadRow";
 import { Link } from "react-router-dom";
 import { useApi } from "../hooks/useApi";
 import { useEffect, useState } from "react";
@@ -24,6 +23,7 @@ const Products = () => {
 
 
   const [products, setProducts] = useState<Product[]>([]);
+  const [itemsLoaded, setItemsLoaded] = useState(30);
   const [loading, setLoading] = useState(true);
   const [retryTimeout, setRetryTimeout] = useState<NodeJS.Timeout | null>(null); // Estado para controlar o intervalo de retentativas de reqTimeout
   const [showConfirmation, setShowConfirmation] = useState(false);
@@ -52,8 +52,8 @@ const Products = () => {
     }
   }
 
-  const handleDelete = async (id: number) => {
-    if (window.confirm("Tem certeza que deseja excluir este produto?")) {
+  const handleDelete = async (id: number, name: string) => {
+    if (window.confirm(`Tem certeza que deseja excluir "${name}"?`)) {
       const response = await api.deleteProduct(id);
       if (response) {
         setConfirmationMessage(response.message);
@@ -74,6 +74,11 @@ const Products = () => {
     }
   }
 
+  const loadMoreItems = () => {
+    setItemsLoaded(itemsLoaded + 15);
+  };
+
+
   useEffect(() => {
     getProducts();
 
@@ -92,123 +97,142 @@ const Products = () => {
       <div className="w-full">
         <Header />
         <div className="p-8 space-y-5">
-          <h2 className="text-2xl font-light text-neutral-800">
-            Gerenciar Produtos
-          </h2>
+          <div className="flex items-center justify-between my-5">
+            <Link to='cadastrar' className="bg-accent text-primary rounded-lg w-fit px-8 py-2 font-semibold flex items-center hover:bg-accent/80">
+              <img src={addicon} alt='add' className='mr-2 brightness-[6]' draggable='false' />
+              Cadastrar Produto
+            </Link>
+            <span className="text-neutral-500 px-2 py-1 float-right">
+              {products.length} produtos
+            </span>
 
+          </div>
 
-          <div>
-            <div className="flex items-center justify-between my-5">
-              <Link to='cadastrar' className="bg-accent text-primary rounded-lg w-fit px-8 py-2 font-semibold flex items-center hover:bg-accent/80">
-                <img src={addicon} alt='add' className='mr-2 brightness-[6]' draggable='false' />
-                Cadastrar Produto
-              </Link>
-              <span className="text-neutral-500 px-2 py-1 float-right">
-                {products.length} produtos
-              </span>
-
+          <div className="w-full">
+            <div className="bg-accent text-sm text-white font-semibold rounded-t-lg rounded-b grid grid-cols-7 py-3 mb-1">
+              <h3 className="px-5 col-span-2">
+                Informações do Produto
+              </h3>
+              <h3 className="px-5">
+                SKU
+              </h3>
+              <h3 className="px-5">
+                Categorias
+              </h3>
+              <h3 className="px-5">
+                Estoque
+              </h3>
+              <h3 className="px-5">
+                Ativo
+              </h3>
             </div>
 
-            <table className="w-full space-y-4">
-              <thead>
-                <tr className="bg-accent rounded-lg border-b text-sm text-neutral-500">
-                  {
-                    loading ? (
-                      <th className="p-4 flex items-center gap-3">
-                        <div className="w-4 h-4 border-2 border-green-300 border-t-transparent rounded-full animate-spin"></div>
-                        <span className="text-white font-medium">Carregando produtos...</span>
-                      </th>
-                    ) : (
-                      <HeadRow content="Informações do Produto" left />
-                    )
-                  }
-                  <HeadRow content="SKU" />
-                  <HeadRow content="Categorias" />
-                  <HeadRow content="Estoque" />
-                  <HeadRow content="Ativo" />
-                  <HeadRow content="" right />
-                </tr>
-              </thead>
-              <tbody className="divide-y space-y-5 bg-white text-left">
-                {
-                  products.length == 0 ? (
-                    <tr className="">
-                      <td className="px-4 py-4">
-                        <span className="font-medium text-red-500 bg-red-200/70 border border-red-500 rounded-lg px-3 py-1 w-fit flex gap-2 items-center">
-                          <img src={erricon} alt="Erro" />
-                          Nenhum produto foi encontrado.
-                        </span>
-                      </td>
-                    </tr>
-                  ) : (
-                    products.slice(0, 30).map((product) => {
-                      const cld = new Cloudinary({
-                        cloud: {
-                          cloudName: 'medellincompany',
-                        }
-                      });
+            <ul className="space-y-1 text-neutral-700">
+              {
+                loading ? (
+                  <div className="bg-green-100 w-fit my-2 px-4 py-2 rounded-lg flex items-center gap-3 shadow-lg shadow-green-500/20">
+                    <div className="w-4 h-4 border-2 border-green-400 border-t-transparent rounded-full animate-spin"></div>
+                    <span className="text-green-500 font-medium">Carregando produtos...</span>
+                  </div>
+                ) : null
+              }
+              {
+                products.length == 0 ? (
+                  <li>
+                    <div className="px-4 py-4">
+                      <span className="font-medium text-red-500 bg-red-200/70 border border-red-500 rounded-lg px-3 py-1 w-fit flex gap-2 items-center">
+                        <img src={erricon} alt="Erro" />
+                        Nenhum produto foi encontrado.
+                      </span>
+                    </div>
+                  </li>
+                ) : (
+                  products.slice(0, itemsLoaded).map((product) => {
+                    const cld = new Cloudinary({
+                      cloud: {
+                        cloudName: 'medellincompany',
+                      }
+                    });
 
-                      const url = cld.image(product.Banner).delivery(quality(auto())).format(auto());
+                    const url = cld.image(product.Banner).delivery(quality(auto())).format(auto());
 
-                      return (
-                        <tr key={product.ID}>
-                          <td className='pl-4 py-3 flex gap-4'>
-                            <AdvancedImage cldImg={url} className="w-14 h-14 rounded-lg object-cover" alt={'Imagem ' + product.Name} loading="lazy" />
-                            <div className='flex flex-col h-fit'>
-                              <span className='font-semibold whitespace-nowrap w-60 overflow-hidden overflow-ellipsis'>
-                                {product.Name}
-                              </span>
-                              <span className="text-xs font-semibold text-neutral-400">
-                                R$ {product.Price}
-                              </span>
-                            </div>
-                          </td>
-                          <td className='pl-4 '>
+                    return (
+                      <li key={product.ID} className="grid grid-cols-7 items-center">
+                        <div className='px-4 flex gap-4 col-span-2 bg-white/80 rounded-l p-2'>
+                          <AdvancedImage cldImg={url} className="w-14 h-14 rounded-lg object-cover" alt={'Imagem ' + product.Name} loading="lazy" />
+                          <div className='flex flex-col h-fit my-auto'>
+                            <span className='font-semibold whitespace-nowrap overflow-hidden overflow-ellipsis md:w-44 2xl:w-64'>
+                              {product.Name}
+                            </span>
+                            <span className="text-xs font-semibold text-neutral-400">
+                              R$ {product.Price}
+                            </span>
+                          </div>
+                        </div>
+                        <div className='bg-white/80 p-2'>
+                          <div className="py-4 px-4">
                             <span>
                               {product.SKU}
                             </span>
-                          </td>
-                          <td className='pl-4 '>
+                          </div>
+                        </div>
+                        <div className='bg-white/80 p-2'>
+                          <div className="py-4 px-4">
                             <span>
                               {product.Category.Name}
                             </span>
-                          </td>
-                          <td className='pl-4 '>
+                          </div>
+                        </div>
+                        <div className='bg-white/80 p-2'>
+                          <div className="py-4 px-4">
                             <span>
                               {product.Stock}
                             </span>
-                          </td>
-                          <td className="pl-4">
-                            <form>
-                              <label htmlFor="active" className="relative inline-flex items-center cursor-pointer">
+                          </div>
+                        </div>
+                        <div className='bg-white/80 p-2'>
+                          <div className="py-4 px-4">
+                            <form className="h-fit">
+                              <label className="relative cursor-pointer">
                                 <input type="checkbox" name="active" id="active" className="sr-only peer" value='' />
                                 <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
                               </label>
                             </form>
-                          </td>
-                          <td className=" relative">
-                            <div className="space-x-4 w-fit mx-auto flex">
-                              <Link to={'/produtos/editar?id=' + product.ID} className="bg-sky-200/80 p-3 rounded-full hover:bg-sky-300/80">
-                                <div>
-                                  <img src={editicon} alt='edit' className='w-4' draggable='false' />
-                                </div>
-                              </Link>
-                              <div onClick={async () => { await handleDelete(product.ID); }} className="bg-red-200/80 p-3 rounded-full cursor-pointer hover:bg-red-300/80">
-                                <img src={deleteicon} alt='delete' className='w-4' draggable='false' />
+                          </div>
+                        </div>
+                        <div className="bg-white/80 relative  rounded-r">
+                          <div className="py-4 space-x-4 w-fit mx-auto flex">
+                            <Link to={'/produtos/editar?id=' + product.ID} className="bg-sky-200/80 p-3 rounded-full hover:bg-sky-300/80">
+                              <div>
+                                <img src={editicon} alt='edit' className='w-4' draggable='false' />
                               </div>
-                              <div className="p-3 rounded-full hover:bg-neutral-300">
-                                <img src={moreicon} alt='more' className='w-4 scale-150' draggable='false' />
-                              </div>
+                            </Link>
+                            <div onClick={async () => { await handleDelete(product.ID, product.Name); }} className="bg-red-200/80 p-3 rounded-full cursor-pointer hover:bg-red-300/80">
+                              <img src={deleteicon} alt='delete' className='w-4' draggable='false' />
                             </div>
-                          </td>
-                        </tr>
-                      )
-                    })
-                  )
-                }
-              </tbody>
-            </table>
+                            <div className="p-3 rounded-full hover:bg-neutral-300">
+                              <img src={moreicon} alt='more' className='w-4 scale-150' draggable='false' />
+                            </div>
+                          </div>
+                        </div>
+                      </li>
+                    )
+                  })
+                )
+              }
+            </ul>
+
           </div>
+          {
+            products.length <= itemsLoaded ? null : (
+              <div className="w-fit">
+                <button className="bg-accent text-white font-medium px-5 py-2 rounded-lg" onClick={loadMoreItems}>
+                  Carregar Mais
+                </button>
+              </div>
+            )
+          }
+
         </div>
         {
           showConfirmation && (
