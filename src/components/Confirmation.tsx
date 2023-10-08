72e0dd471bd5d0578baf5563ad6890d37297680a
Vini <vsgh-gouveia@hotmail.com>
2023-10-08 14:34:50 -0300

    Div desnecessária removida em Confirmation.tsx

--- src/components/Confirmation.tsx
+++ src/components/Confirmation.tsx
@@ -6,16 +6,14 @@ type Props = {
 
 const Confirmation = (props: Props) => {
   return (
-    <div className="">
-      <motion.div
-        initial={{ opacity: 0, y: 50 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
-        className="w-[87%] fixed bottom-0 right-0 bg-green-500 text-white text-center py-2 rounded-es-2xl"
-      >
-        {props.content}
-      </motion.div>
-    </div>
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className="w-[87%] fixed bottom-0 right-0 bg-green-500 text-white text-center py-2"
+    >
+      {props.content}
+    </motion.div>
   )
 }
 
