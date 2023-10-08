0e61e20c0ba680f0042ac681bcfc33f26c484ae0
Vini <vsgh-gouveia@hotmail.com>
2023-10-08 20:02:31 -0300

    Navegar para página anterior quando na pag 404

--- src/pages/err/NotFound.tsx
+++ src/pages/err/NotFound.tsx
@@ -1,15 +1,19 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
+  const navigate = useNavigate();
 
+  const back = () => {
+    navigate(-1);
+  }
 
   return (
     <div>
 
       <div className="h-screen">
-        <Link to={'/dashboard'}>
+        <button onClick={back}>
           voltar
-        </Link>
+        </button>
       </div>
     </div>
   )
