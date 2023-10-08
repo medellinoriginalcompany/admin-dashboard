aece16ff8dbc0edaa36d3054daee908d836de32a
Vini <vsgh-gouveia@hotmail.com>
2023-10-08 14:44:24 -0300

    Props para RequireAuth

--- src/components/page/DefaultPage.tsx
+++ src/components/page/DefaultPage.tsx
@@ -3,7 +3,7 @@ import Header from './Header';
 import Sidebar from './Sidebar';
 
 type DefaultPageProps = {
-  children: React.JSX.Element | React.JSX.Element[];
+  children: React.JSX.Element | React.JSX.Element[]; // Renderiza um elemento ou um array de elementos JSX
 };
 
 const DefaultPage = ({ children }: DefaultPageProps) => {
--- src/contexts/RequireAuth.tsx
+++ src/contexts/RequireAuth.tsx
@@ -2,7 +2,11 @@ import React, { useContext } from 'react'
 import { AuthContext } from './AuthContext'
 import Login from '../pages/Login';
 
-export const RequireAuth = ({ children }: {children: React.JSX.Element}) => {
+type RequireAuthProps = {
+  children: React.JSX.Element// Renderiza um elemento ou um array de elementos JSX
+};
+
+export const RequireAuth = ({ children }: RequireAuthProps) => {
   const auth = useContext(AuthContext);
 
   // Se o usuário não estiver logado, redireciona para a página de login
