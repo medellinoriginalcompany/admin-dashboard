6115f43be52e11d52f0a82758c7fa60d4fa1ca52
Vini <vsgh-gouveia@hotmail.com>
2023-10-08 23:22:12 -0300

    Mensagem e função para restaurar produto

--- src/components/TrashCard.tsx
+++ src/components/TrashCard.tsx
@@ -18,30 +18,61 @@ const TrashCard = (props: Props) => {
   const api = useApi();
 
   const [showOptions, setShowOptions] = useState<boolean>(false);
-  const [showConfirmation, setShowConfirmation] = useState<boolean>(false);
+  const [showConfirmationRestore, setShowConfirmationRestore] = useState<boolean>(false);
+  const [showConfirmationDelete, setShowConfirmationDelete] = useState<boolean>(false);
   const [confirmationMessage, setConfirmationMessage] = useState<string>("");
   const [errMsg, setErrMsg] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
 
   const trashCardRef = useRef<HTMLDivElement>(null);
-  const confirmationRef = useRef<HTMLDivElement>(null);
+  const confirmationDeleteRef = useRef<HTMLDivElement>(null);
+  const confirmationRestoreRef = useRef<HTMLDivElement>(null);
 
   const handleClickOutside = (event: { target: any; }) => {
     if (trashCardRef.current && !trashCardRef.current.contains(event.target)) {
       setShowOptions(false);
     }
 
-    if (confirmationRef.current && !confirmationRef.current.contains(event.target)) {
-      setShowConfirmation(false);
+    if (confirmationDeleteRef.current && !confirmationDeleteRef.current.contains(event.target)) {
+      setShowConfirmationDelete(false);
+    }
+
+    if (confirmationRestoreRef.current && !confirmationRestoreRef.current.contains(event.target)) {
+      setShowConfirmationRestore(false);
     }
   };
 
   const handleEscape = (event: { keyCode: number; }) => {
     if (event.keyCode === 27) {
       setShowOptions(false);
-      setShowConfirmation(false);
+      setShowConfirmationDelete(false);
+      setShowConfirmationRestore(false);
     }
   };
+
+  const handleRestore = async (id: number) => {
+    try {
+      setLoading(true);
+      const response = await api.restoreProduct(id);
+
+      if (response) {
+        setLoading(false);
+        setConfirmationMessage(response.message);
+
+        setTimeout(() => {
+          window.location.reload();
+        }, 1100);
+      }
+    } catch (error: any) {
+      setLoading(false);
+      setConfirmationMessage('');
+      setErrMsg(error.response.data.message);
+
+      // setTimeout(() => {
+      //   window.location.reload();
+      // }, 5000);
+    }
+  }
 
   const handleDelete = async (id: number) => {
     try {
@@ -77,7 +108,7 @@ const TrashCard = (props: Props) => {
 
   useEffect(() => {
     // Adiciona o ouvinte de eventos quando showOptions for verdadeiro
-    if (showOptions || showConfirmation) {
+    if (showOptions || showConfirmationDelete || showConfirmationRestore) {
       document.addEventListener('mousedown', handleClickOutside);
     } else {
       // Remove o ouvinte de eventos quando showOptions for falso
@@ -88,7 +119,7 @@ const TrashCard = (props: Props) => {
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
-  }, [showOptions, showConfirmation]);
+  }, [showOptions, showConfirmationDelete, showConfirmationRestore]);
 
   return (
     <>
@@ -109,8 +140,9 @@ const TrashCard = (props: Props) => {
           showOptions && (
             <div className='absolute flex-col right-1 top-10 bg-accent/10 backdrop-blur rounded z-20'>
               <div className="flex flex-col w-fit self-end divide-y divide-blue-200">
-                <button className="text-sm text-end text-blue-500 font-medium py-1 px-2 rounded-sm hover:bg-blue-500/80 hover:text-white">Restaurar</button>
-                <button onClick={() => setShowConfirmation(true)}
+                <button onClick={() => setShowConfirmationRestore(true)}
+                  className="text-sm text-end text-blue-500 font-medium py-1 px-2 rounded-sm hover:bg-blue-500/80 hover:text-white">Restaurar</button>
+                <button onClick={() => setShowConfirmationDelete(true)}
                   className="text-sm text-end text-red-500 font-medium py-1 px-2 rounded-sm hover:bg-red-500/80 hover:text-white">Deletar</button>
               </div>
             </div>
@@ -118,10 +150,10 @@ const TrashCard = (props: Props) => {
         }
       </div>
       {
-        showConfirmation && (
+        showConfirmationDelete && (
           <div className='absolute top-0 left-0 w-full h-full bg-neutral-800/20 z-50'>
             <div className='flex flex-col justify-center h-full'>
-              <div className='bg-white max-w-4xl mx-auto rounded' ref={confirmationRef}>
+              <div className='bg-white max-w-4xl mx-auto rounded' ref={confirmationDeleteRef}>
                 <div className="flex flex-col px-9 py-9 space-y-10">
                   <div className=''>
                     <h2 className='text-2xl font-semibold flex items-center gap-2'>
@@ -171,8 +203,69 @@ const TrashCard = (props: Props) => {
                         </div>
                       )
                     }
-                    <button className='bg-accent text-white font-medium px-4 py-2 rounded hover:bg-blue-700' onClick={() => setShowConfirmation(false)}>Não! Me leve de volta</button>
+                    <button onClick={() => setShowConfirmationDelete(false) }
+                      className='bg-accent text-white font-medium px-4 py-2 rounded hover:bg-blue-700' >Não! Me leve de volta</button>
                     <button className='font-semibold text-red-500 w-fit mx-auto' onClick={() => handleDelete(props.id)}>Deletar permanentemente</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )
+      }
+      {
+        showConfirmationRestore && (
+          <div className='absolute top-0 left-0 w-full h-full bg-neutral-800/20 z-50'>
+            <div className='flex flex-col justify-center h-full'>
+              <div className='bg-white max-w-4xl mx-auto rounded' ref={confirmationRestoreRef}>
+                <div className="flex flex-col px-9 py-9 space-y-10">
+                  <div className=''>
+                    <h3 className='text-xl font-medium'>
+                      Tem certeza que deseja restaurar este produto?
+                    </h3>
+
+                    <p className='mx-auto w-fit my-3 bg-neutral-200 px-2 rounded whitespace-pre'>{props.name}</p>
+                  </div>
+
+                  <div className='flex flex-col space-y-2'>
+                    {
+                      loading && (
+                        <div className='bg-blue-100 px-4 py-2 rounded flex justify-between items-center'>
+                          <p className='text-blue-500 font-medium text-center'>
+                            Carregando...
+                          </p>
+                          <div className='w-4 h-4 rounded-full border-2 border-blue-500 border-t-transparent animate-spin'></div>
+                        </div>
+                      )
+                    }
+                    {
+                      confirmationMessage && (
+                        <motion.div className='bg-green-100 px-4 py-2 rounded'
+                          initial={{ maxHeight: '0%', opacity: 0, translateY: 50 }}
+                          animate={{ maxHeight: '100%', opacity: 1, translateY: 0 }}
+                          transition={{
+                            duration: 1,
+                            type: 'spring',
+                          }}
+                        >
+                          <p className='text-green-500 font-medium text-center'>
+                            {confirmationMessage}
+                          </p>
+                        </motion.div>
+                      )
+                    }
+                    {
+                      errMsg && (
+                        <div className='bg-red-100 px-4 py-2 rounded flex justify-between items-center'>
+                          <p className='text-red-500 font-medium text-center'>
+                            {errMsg}
+                          </p>
+                        </div>
+                      )
+                    }
+                    <button onClick={() => setShowConfirmationRestore(false) }
+                      className='bg-accent text-white font-medium px-4 py-2 rounded hover:bg-blue-700' >Não! Me leve de volta</button>
+                    <button className='font-semibold text-red-500 w-fit mx-auto' onClick={() => handleRestore(props.id)}>Restaurar produto</button>
                   </div>
                 </div>
               </div>
