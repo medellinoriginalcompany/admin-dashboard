e5b4647a68ff6dd02ae2a069c5dab580f50ab260
Vini <vsgh-gouveia@hotmail.com>
2023-10-09 11:19:31 -0300

    AINDA AJUSTAR - Componentização de cards de produtos

---
+++ src/components/products/ProductCard.tsx
@@ -0,0 +1,136 @@
+import { AdvancedImage } from "@cloudinary/react";
+import { Link } from "react-router-dom";
+import { CloudinaryImage } from "@cloudinary/url-gen/index";
+import { Product } from "../../types/product/Product";
+import { useState } from "react";
+
+import editicon from '/icons/edit.svg';
+import deleteicon from '/icons/trash.svg';
+import moreicon from '/icons/more.svg';
+import Confirmation from "../Confirmation";
+import { useApi } from "../../hooks/useApi";
+
+type Props = {
+  product: Product,
+  imageURL: CloudinaryImage,
+}
+
+const ProductCard = (props: Props) => {
+
+  const api = useApi();
+
+  const [showImage, setShowImage] = useState(false);
+  const [showConfirmation, setShowConfirmation] = useState(false);
+  const [confirmationMessage, setConfirmationMessage] = useState("");
+
+
+  const handleDelete = async (id: number, name: string) => {
+    if (window.confirm(`Tem certeza que deseja excluir "${name}"?`)) {
+      const response = await api.deleteProduct(id);
+      if (response) {
+        setConfirmationMessage(response.message);
+        setShowConfirmation(true);
+        setTimeout(() => {
+          setShowConfirmation(false);
+        }, 5000);
+
+      } else {
+        console.log("Ocorreu um erro ao excluir o produto");
+        setConfirmationMessage("Ocorreu um erro ao excluir o produto");
+        setShowConfirmation(true);
+      }
+
+
+    }
+  }
+
+  
+
+  return (
+    <>
+      <li key={props.product.Name} className="grid grid-cols-7 items-center">
+        <div className='px-4 flex gap-4 col-span-2 bg-white/80 rounded-l p-2'>
+          <div onClick={() => setShowImage(true)}>
+            <AdvancedImage cldImg={props.imageURL} className="w-14 min-w-[56px] h-14 rounded-lg object-cover" alt={'Imagem ' + props.product.Name} loading="lazy" />
+          </div>
+          <Link to={'/produtos/editar?id=' + props.product.ID} className='flex flex-col h-fit my-auto'>
+            <span className='font-semibold whitespace-nowrap overflow-hidden overflow-ellipsis md:w-44 2xl:w-80' title={props.product.Name}>
+              {props.product.Name}
+            </span>
+            <span className="text-xs font-semibold text-neutral-400">
+              R$ {props.product.Price}
+            </span>
+          </Link>
+        </div>
+        <div className='bg-white/80 p-2'>
+          <div className="py-4 px-4">
+            <span>
+              {props.product.SKU}
+            </span>
+          </div>
+        </div>
+        <div className='bg-white/80 p-2'>
+          <div className="py-4 2xl:px-4">
+            <span className="overflow-hidden whitespace-nowrap text-ellipsis">
+              {props.product.Category.Name}
+            </span>
+          </div>
+        </div>
+        <div className='bg-white/80 p-2'>
+          <div className="py-4 px-4">
+            <span>
+              {props.product.Stock}
+            </span>
+          </div>
+        </div>
+        <div className='bg-white/80 p-2'>
+          <div className="py-4 px-4">
+            <form className="h-fit">
+              <label className="relative cursor-pointer">
+                <input type="checkbox" name="active" id="active" className="sr-only peer" value='' />
+                <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
+              </label>
+            </form>
+          </div>
+        </div>
+        <div className="bg-white/80 relative  rounded-r">
+          <div className="py-4 space-x-4 w-fit mx-auto flex">
+            <Link to={'/produtos/editar?id=' + props.product.ID} className="bg-sky-200/80 p-3 rounded-full hover:bg-sky-300/80">
+              <div>
+                <img src={editicon} alt='edit' className='w-4' draggable='false' />
+              </div>
+            </Link>
+            <div onClick={async () => { await handleDelete(props.product.ID, props.product.Name); }} className="bg-red-200/80 p-3 rounded-full cursor-pointer hover:bg-red-300/80">
+              <img src={deleteicon} alt='delete' className='w-4' draggable='false' />
+            </div>
+            <div className="p-3 rounded-full hover:bg-neutral-300">
+              <img src={moreicon} alt='more' className='w-4 scale-150' draggable='false' />
+            </div>
+          </div>
+        </div>
+      </li>
+      {
+        showImage && (
+          <div className='fixed -top-1 left-0 w-full h-full bg-neutral-800/20 z-50' onClick={() => setShowImage(false)}>
+            <div className='flex flex-col justify-center h-full'>
+              <div className="max-w-4xl mx-auto">
+                <AdvancedImage cldImg={url} alt="" />
+              </div>
+            </div>
+          </div>
+        )
+      }
+      <>
+        {
+          showConfirmation && (
+            <Confirmation content={confirmationMessage} />
+          )
+        }
+
+      </>
+    </>
+
+  )
+}
+
+export default ProductCard
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -2,17 +2,14 @@ import { Link } from "react-router-dom";
 import { useApi } from "../hooks/useApi";
 import { useEffect, useState } from "react";
 import { Product } from "../types/product/Product";
+import { Cloudinary } from "@cloudinary/url-gen/index";
+import { auto } from "@cloudinary/url-gen/qualifiers/quality";
 
-import addicon from '/icons/add-square.svg';
-import editicon from '/icons/edit.svg';
-import deleteicon from '/icons/trash.svg';
-import moreicon from '/icons/more.svg';
+import addicon from '/icons/add-square.svg';  
 import Confirmation from "../components/Confirmation";
 import erricon from '/icons/danger-red.svg';
-import { Cloudinary } from "@cloudinary/url-gen/index";
-import { AdvancedImage } from "@cloudinary/react";
-import { auto } from "@cloudinary/url-gen/qualifiers/quality";
 import DefaultPage from "../components/page/DefaultPage";
+import ProductCard from "../components/products/ProductCard";
 
 
 
@@ -26,7 +23,6 @@ const Products = () => {
   const [retryTimeout, setRetryTimeout] = useState<NodeJS.Timeout | null>(null); // Estado para controlar o intervalo de retentativas de reqTimeout
   const [showConfirmation, setShowConfirmation] = useState(false);
   const [confirmationMessage, setConfirmationMessage] = useState("");
-
   const api = useApi();
 
   const getProducts = async () => {
@@ -47,28 +43,6 @@ const Products = () => {
         getProducts();
       }, 5000);
       setRetryTimeout(timeout);
-    }
-  }
-
-  const handleDelete = async (id: number, name: string) => {
-    if (window.confirm(`Tem certeza que deseja excluir "${name}"?`)) {
-      const response = await api.deleteProduct(id);
-      if (response) {
-        setConfirmationMessage(response.message);
-        setShowConfirmation(true);
-        setTimeout(() => {
-          setShowConfirmation(false);
-        }, 5000);
-        // Reload products
-        getProducts();
-
-      } else {
-        console.log("Ocorreu um erro ao excluir o produto");
-        setConfirmationMessage("Ocorreu um erro ao excluir o produto");
-        setShowConfirmation(true);
-      }
-
-
     }
   }
 
@@ -149,67 +123,16 @@ const Products = () => {
                   });
 
                   const url = cld.image(product.Banner).format(auto());
+          
 
                   return (
-                    <li key={product.ID} className="grid grid-cols-7 items-center">
-                      <div className='px-4 flex gap-4 col-span-2 bg-white/80 rounded-l p-2'>
-                        <AdvancedImage cldImg={url} className="w-14 min-w-[56px] h-14 rounded-lg object-cover" alt={'Imagem ' + product.Name} loading="lazy" />
-                        <Link to={'/produtos/editar?id=' + product.ID} className='flex flex-col h-fit my-auto'>
-                          <span className='font-semibold whitespace-nowrap overflow-hidden overflow-ellipsis md:w-44 2xl:w-80' title={product.Name}>
-                            {product.Name}
-                          </span>
-                          <span className="text-xs font-semibold text-neutral-400">
-                            R$ {product.Price}
-                          </span>
-                        </Link>
-                      </div>
-                      <div className='bg-white/80 p-2'>
-                        <div className="py-4 px-4">
-                          <span>
-                            {product.SKU}
-                          </span>
-                        </div>
-                      </div>
-                      <div className='bg-white/80 p-2'>
-                        <div className="py-4 2xl:px-4">
-                          <span>
-                            {product.Category.Name}
-                          </span>
-                        </div>
-                      </div>
-                      <div className='bg-white/80 p-2'>
-                        <div className="py-4 px-4">
-                          <span>
-                            {product.Stock}
-                          </span>
-                        </div>
-                      </div>
-                      <div className='bg-white/80 p-2'>
-                        <div className="py-4 px-4">
-                          <form className="h-fit">
-                            <label className="relative cursor-pointer">
-                              <input type="checkbox" name="active" id="active" className="sr-only peer" value='' />
-                              <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
-                            </label>
-                          </form>
-                        </div>
-                      </div>
-                      <div className="bg-white/80 relative  rounded-r">
-                        <div className="py-4 space-x-4 w-fit mx-auto flex">
-                          <Link to={'/produtos/editar?id=' + product.ID} className="bg-sky-200/80 p-3 rounded-full hover:bg-sky-300/80">
-                            <div>
-                              <img src={editicon} alt='edit' className='w-4' draggable='false' />
-                            </div>
-                          </Link>
-                          <div onClick={async () => { await handleDelete(product.ID, product.Name); }} className="bg-red-200/80 p-3 rounded-full cursor-pointer hover:bg-red-300/80">
-                            <img src={deleteicon} alt='delete' className='w-4' draggable='false' />
-                          </div>
-                          <div className="p-3 rounded-full hover:bg-neutral-300">
-                            <img src={moreicon} alt='more' className='w-4 scale-150' draggable='false' />
-                          </div>
-                        </div>
-                      </div>
-                    </li>
+                    <>
+                      <ProductCard
+                        key={product.ID}
+                        imageURL={url}
+                        product={product}
+                      />
+                    </>
                   )
                 })
               )
