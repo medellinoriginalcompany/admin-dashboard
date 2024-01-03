0452e567a8e3c19dab175834327992fe425e3a32
Vini <vsgh-gouveia@hotmail.com>
2024-01-03 01:06:48 -0300

    Alterações importantes na Sidebar
    Header foi removida da área útil da dashboard, informações sobre o perfil e copyright adicionados ao rodapé.

--- src/components/page/Sidebar.tsx
+++ src/components/page/Sidebar.tsx
@@ -1,26 +1,55 @@
 import { Link } from 'react-router-dom'
 import NavItem from './NavItem'
-import { useContext } from 'react'
+import { useContext, useEffect, useRef, useState } from 'react'
 import { AuthContext } from '../../contexts/AuthContext'
 
 import logo from '/images/medellin-black.png'
+import usericon from '/images/user_icon.jpg';
+import moreicon from '/icons/more.svg';
+import notificationicon from '/icons/notification.svg';
 
 const Sidebar = () => {
   const auth = useContext(AuthContext);
+
+  const [profileOptions, setProfileOptions] = useState(false);
+  const profileRef = useRef<HTMLDivElement>(null);
+
   const handleLogout = () => {
     if (window.confirm('Deseja realmente sair?')) {
       auth.logout();
     }
   }
 
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (profileRef.current && !profileRef.current.contains(event.target as Node)) {
+        setProfileOptions(false);
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [setProfileOptions]);
+
   return (
-    <div className=" sticky top-0 lg:min-w-[280px] 2xl:min-w-[310px] h-screen z-50 bg-neutral-50 border-r border-neutral-200">
+    <div className="sticky top-0 lg:min-w-[280px] 2xl:min-w-[310px] h-screen z-50 bg-neutral-50 border-r border-neutral-200">
       <nav className='flex flex-col justify-between h-full'>
         <div className='space-y-7'>
-          <div className='mt-8 mb-24 w-fit'>
-            <Link to='/dashboard'>
-              <img src={logo} alt="Logo" className='w-24 ml-10' />
-            </Link>
+          <div className='flex items-center justify-between mx-9 mt-8 mb-24'>
+            <div className='w-fit'>
+              <Link to='/dashboard'>
+                <img src={logo} alt="Logo" className='w-24' />
+              </Link>
+            </div>
+            <div className='relative flex items-center space-x-2'>
+              <img src={notificationicon} alt='bell' className='w-7' />
+              <span className='bg-red-500 rounded-full text-[10px] text-primary text-center w-4 h-4 absolute top-0 right-0 font-semibold'>
+                8
+              </span>
+            </div>
           </div>
 
           <ul className='space-y-2 px-9'>
@@ -62,18 +91,50 @@ const Sidebar = () => {
             </li>
           </ul>
         </div>
-        <div className='px-8 py-10'>
-          <button
-            className='flex items-center w-full space-x-2 px-8 py-1.5 rounded-md bg-neutral-100 border border-neutral-300 hover:bg-neutral-200'
-            onClick={handleLogout}>
-            <div className=''>
-              <img src={`/icons/logout.svg`} alt='logout' className='w-5 transition-all' />
+
+        <div>
+          {profileOptions && (
+            <div className='profile my-2 py-3 px-3 ml-9 mr-3 space-y-3 rounded bg-neutral-50 border border-neutral-300'>
+              <button
+                className='flex items-center justify-center w-full space-x-2 px-8 py-1.5 rounded bg-neutral-100 border border-neutral-300 hover:bg-neutral-200'>
+
+                <span className='font-semibold text-sm text-neutral-700'>
+                  Alterar foto de perfil
+                </span>
+              </button>
+
+              <button onClick={handleLogout}
+                className='flex items-center justify-center w-full space-x-2 px-8 py-1.5 rounded bg-red-50 hover:bg-red-100'>
+
+                <span className='font-semibold text-sm text-red-500'>
+                  Sair da conta
+                </span>
+              </button>
+            </div>
+          )}
+
+          <div className='bg-neutral-100 border-t border-neutral-200 px-2 pt-3' ref={profileRef}>
+            <div onClick={() => setProfileOptions(!profileOptions)}
+              className='profile flex items-center justify-between px-4 py-2 mb-5 rounded cursor-pointer hover:bg-neutral-200'>
+              <div className='flex items-center gap-3'>
+                <img src={usericon} alt='user' className='w-8 h-8 object-cover rounded-full' />
+                <div className='flex flex-col'>
+                  <span className='text-sm font-semibold text-neutral-800'>
+                    {auth.user?.FirstName + ' ' + auth.user?.LastName}
+                  </span>
+                  <span title={auth.user?.Email} className='text-xs text-neutral-400 overflow-x-hidden text-ellipsis whitespace-nowrap w-40'>
+                    {auth.user?.Email}
+                  </span>
+                </div>
+              </div>
+              <img src={moreicon} alt="" />
             </div>
 
-            <span className='font-semibold text-neutral-700'>
-              Sair
-            </span>
-          </button>
+            <p className='text-xs text-center mb-4 text-neutral-500'>
+              &copy; Medellin Original Company 2024. <br />Todos os direitos reservados.
+            </p>
+          </div>
+
         </div>
       </nav>
     </div>
