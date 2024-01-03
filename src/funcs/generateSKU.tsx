2145f84330760ec0f8619666ac0fc4c609b9224d
Vini <vsgh-gouveia@hotmail.com>
2024-01-02 21:48:35 -0300

    Alterado a forma como o SKU é gerado

--- src/funcs/generateSKU.tsx
+++ src/funcs/generateSKU.tsx
@@ -1,22 +1,22 @@
 
-const generateSKU = (manufact: string, type: string, color: string, size: string, category: string,) => { // TODO: Adicionar o parâmetro de fabricante
+const generateSKU = (manufact: string, type: string, category: string,) => {
   // Pegar as três primeiras letras do fabricante
   const firstLetter = manufact.charAt(0) + manufact.charAt(1) + manufact.charAt(2);
 
   // Pegar as três primeiras letras do tipo
   const secondLetter = type.charAt(0) + type.charAt(1) + type.charAt(2);
-
-  // Pegar as quatro primeiras letras da cor
-  const thirdLetter = color.charAt(0) + color.charAt(1) + color.charAt(2);
-
-  // Pegar as duas primeiras letras do tamanho
-  const fourthLetter = size.charAt(0) + size.charAt(1) + size.charAt(2);
   
   // Pegar as três primeiras letras da categoria
   const fifthLetter = category.charAt(0) + category.charAt(1) + category.charAt(2);
 
+  // Gerar data como 20240102
+  const date = new Date();
+  const year = date.getFullYear().toString();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Adiciona um zero à esquerda, se necessário
+  const day = date.getDate().toString().padStart(2, '0'); // Adiciona um zero à esquerda, se necessário
+
   // Gerar SKU em caixa alta
-  const sku = (firstLetter + secondLetter + thirdLetter + fourthLetter + fifthLetter).toUpperCase();
+  const sku = (firstLetter + secondLetter + fifthLetter).toUpperCase() + year + month + day;
 
   return sku;
 }
