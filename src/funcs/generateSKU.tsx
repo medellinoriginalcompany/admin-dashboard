2dff1a9d4ec8c0d7eb9d30b4cea87166e7083628
Vini <vsgh-gouveia@hotmail.com>
2024-01-03 12:14:07 -0300

    Forma como SKU é gerado alterado

--- src/funcs/generateSKU.tsx
+++ src/funcs/generateSKU.tsx
@@ -1,13 +1,19 @@
 
-const generateSKU = (manufact: string, type: string, category: string,) => {
-  // Pegar as três primeiras letras do fabricante
-  const firstLetter = manufact.charAt(0) + manufact.charAt(1) + manufact.charAt(2);
+const generateSKU = (manufact: string, type: string, category: string, color: string, print: string) => {
+  // Três primeiras letras do fabricante
+  const first = manufact.charAt(0) + manufact.charAt(1) + manufact.charAt(2);
 
-  // Pegar as três primeiras letras do tipo
-  const secondLetter = type.charAt(0) + type.charAt(1) + type.charAt(2);
-  
-  // Pegar as três primeiras letras da categoria
-  const fifthLetter = category.charAt(0) + category.charAt(1) + category.charAt(2);
+  // Três primeiras letras do tipo
+  const second = type.charAt(0) + type.charAt(1) + type.charAt(2);
+
+  // Três primeiras letras da categoria
+  const third = category.charAt(0) + category.charAt(1) + category.charAt(2);
+
+  // Quatro primeiras letras da cor
+  const fourth = color.charAt(0) + color.charAt(1) + color.charAt(2) + color.charAt(3);
+
+  // Três primeiras letras da estampa
+  const fifth = print.charAt(0) + print.charAt(1) + print.charAt(2);
 
   // Gerar data como 20240102
   const date = new Date();
@@ -16,7 +22,7 @@ const generateSKU = (manufact: string, type: string, category: string,) => {
   const day = date.getDate().toString().padStart(2, '0'); // Adiciona um zero à esquerda, se necessário
 
   // Gerar SKU em caixa alta
-  const sku = (firstLetter + secondLetter + fifthLetter).toUpperCase() + year + month + day;
+  const sku = (first + second + third + fourth + fifth).toUpperCase() + year + month + day;
 
   return sku;
 }
