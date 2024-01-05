771a9184d7dc06ba224f863014e456f5bcc435a4
Vini <vsgh-gouveia@hotmail.com>
2024-01-05 02:42:14 -0300

    variáveis tailwind de dark mode adicionado e outras removidas

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,16 @@
 export default {
   mode: 'jit',
   content: [
-    "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: 'class',
   theme: {
     extend: {
       colors: {
-        'gradient-primary': '#ecf2fe',
-        'gradient-secondary': '#dae4f0',
         'accent': '#22201e',
-        'primary': '#fff', 
-        'secondary': '#F7F7FA',
-        'tertiary': '#d4d4d4',
+        'accent-dark': '#fafafa',
+        'neutral-915': '#151515',
+        'neutral-925': '#101010',
       },
       boxShadow: {
         'b': '0px 10px 15px -15px rgba(0, 0, 0, 0.3)',
